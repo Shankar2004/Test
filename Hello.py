abd07a999b8a9f7a7a4e72cf8b6e84e22d3cd93f
Shankar2004 <95488315+Shankar2004@users.noreply.github.com>
2021-12-22 22:01:16 +0530

    Python file

--- Hello.py
+++ Hello.py
@@ -1,1 +1,15 @@
+#python gives us a option to check if the class is an instance or not using the isinstance(instance,Class) keyword
+class Parent:
+    pass
+class Wizard(Parent):
+    def __init__(self,name,power):
+        self.name = name
+        self.power = power
 
+wizard1 = Wizard('Andrew',200)
+
+print(isinstance(wizard1,Wizard))   #isinstance(instance,class)
+print(isinstance(wizard1,Parent))   #child class is also a instance of Parent class
+
+
+  
